886870188107ac54bfec69ae6691097a6435c555
javierv <javierv@eventbrite.com>
2019-09-10 09:56:43 -0300

    changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,55 +5,62 @@ import MapSvg from 'eventbrite_design_system/iconography/icons/Map';
 import ExpansionPanel from 'eventbrite_design_system/expansionPanel/ExpansionPanel';
 
 const items = [
-    {
-        content: 'Grid exercise I',
-        value: 'ex1',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Grid exercise II',
-        value: 'ex2',
-        iconType: <MapSvg />,
-    },
+    // {
+    //     content: 'Grid exercise I',
+    //     value: 'ex1',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Grid exercise II',
+    //     value: 'ex2',
+    //     iconType: <MapSvg />,
+    // },
 ];
 
 const hocItems = [
-    {
-        content: 'Basic Structure',
-        value: 'structure/basicStructure',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Notifications',
-        value: 'structure/hoc/mainControls/notification',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Action Bar',
-        value: 'structure/hoc/mainControls/actionBar',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Overlay Modal',
-        value: 'structure/hoc/overlayControls/overlayModal',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Overlay Dialog',
-        value: 'structure/hoc/overlayControls/overlayDialog',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Focus Drawer',
-        value: 'structure/hoc/focusDrawer/',
-        iconType: <MapSvg />,
-    },
-    {
-        content: 'Focus Drawer with router',
-        value: 'routerStructure/hoc/focusDrawer',
-        iconType: <MapSvg />,
-    },
+    // {
+    //     content: 'Basic Structure',
+    //     value: 'structure/basicStructure',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Notifications',
+    //     value: 'structure/hoc/mainControls/notification',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Action Bar',
+    //     value: 'structure/hoc/mainControls/actionBar',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Overlay Modal',
+    //     value: 'structure/hoc/overlayControls/overlayModal',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Overlay Dialog',
+    //     value: 'structure/hoc/overlayControls/overlayDialog',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Focus Drawer',
+    //     value: 'structure/hoc/focusDrawer/',
+    //     iconType: <MapSvg />,
+    // },
+    // {
+    //     content: 'Focus Drawer with router',
+    //     value: 'routerStructure/hoc/focusDrawer',
+    //     iconType: <MapSvg />,
+    // },
 ];
+const formItems = [
+    // {
+    //     content: 'Redux Form Basic',
+    //     value: 'reduxForm/basic',
+    //     iconType: <MapSvg />,
+    // },
+]
 
 const navigate = (route) => {
     browserHistory.push(route);
@@ -61,7 +68,7 @@ const navigate = (route) => {
 
 const Landing = ({history}) => (
     <>
-        <ExpansionPanel
+        {/* <ExpansionPanel
             linkText="General CSS"
             verticalMargin="both"
             //eslint-disable-next-line react/style-prop-object
@@ -77,7 +84,16 @@ const Landing = ({history}) => (
             style="container"
         >
             <TextList items={hocItems} onItemSelect={navigate}/>
-        </ExpansionPanel>        
+        </ExpansionPanel>
+
+        <ExpansionPanel
+            linkText="Redux Form"
+            verticalMargin="both"
+            //eslint-disable-next-line react/style-prop-object
+            style="container"
+        >
+            <TextList items={formItems} onItemSelect={navigate}/>
+        </ExpansionPanel> */}
     </>
 );
 
